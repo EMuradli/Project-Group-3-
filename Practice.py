d1c6ad2f0b81ca0046f8d8ba031ccc53918e8080
Emil Muradli <129963266+EMuradli@users.noreply.github.com>
2023-04-20 14:48:46 +0200

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -1,30 +1,14 @@
-import chemparse
-print("NOTICE: \nThis program is case sensitive.")
+import chemparse as chem
 def element_balancer():
-    reactants_list=[]
-    products_list = []
-    
-    num_reactants = int(input("How many reactants do you have?: "))
-    for i in range(num_reactants):
-        reactants=input("Enter the reactants: ")
-        reactants_dict=chemparse.parse_formula(reactants)
-        for i in range(len(reactants_dict.keys())):
-         reactant_list_element = list(reactants_dict.keys())
-         reactants_list.append(reactant_list_element[i])
-
-    num_products = int(input("How many products do you have?: "))
-    for i in range(num_products):
-        products=input("Enter the products: ")
-        products_dict = chemparse.parse_formula(products)
-        for i in range(len(products_dict.keys())):
-            product_list_element=list(products_dict.keys())
-            products_list.append(product_list_element[i])
-    
-
-
-    return reactants_list,products_list
-r_list,p_list = element_balancer()
-print(r_list)
-print(p_list)
-
-
+    react_list = []
+    prod_list = []
+    reactant_num = int(input("How many reactants do you have? "))
+    for i in range(reactant_num):
+        reactants = react_list.append(chem.parse_formula(input("Enter the reactants: ")))
+    product_num = int(input("How many products do you have? "))
+    for i in range(product_num):
+        products = prod_list.append(chem.parse_formula(input("Enter the products: ")))
+    return react_list,prod_list
+react_list,prod_list = element_balancer()
+print(react_list)
+print(prod_list)
