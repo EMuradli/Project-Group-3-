aa8fa1553ee2ede6708472e1542f6cffde134965
wsnggni <131288277+wsnggni@users.noreply.github.com>
2023-04-20 13:39:12 +0200

    Update Wisang's code, with Gaussian Elimination.py

--- Wisang's code, with Gaussian Elimination.py
+++ Wisang's code, with Gaussian Elimination.py
@@ -1,53 +1,49 @@
-
-# A basic code for matrix input from user
-
+#Matrix input
 R = int(input("Enter the number of rows: "))
 C = int(input("Enter the number of columns: "))
 # Initialize matrix
 matrix = []
-print("Enter the entries rowwise:")
+print("Enter the entries row-wise:")
 
 # For user input
-for i in range(R):		 # A for loop for row entries
-	a = []
-	for j in range(C):	 # A for loop for column entries
-		a.append(int(input()))
-	matrix.append(a)
+for i in range(R):      #Loop for rows
+    a = []
+    for j in range(C):      #Loop for columns
+        a.append(int(input()))
+    matrix.append(a)
 
 b = list(map(int, input("Enter your vector, separated by spaces: ").split()))
 
-# For printing the matrix
+#Printing matrix
 for i in range(R):
-	for j in range(C):
-		print(matrix[i][j], end = " ")
-	print()
+    for j in range(C):
+        print(matrix[i][j], end=" ")
+    print()
 
 A = matrix
 
 def gaussian_elimination(A, b):
     n = len(A)
 
-    # Augment the matrix A with the vector b
+    #Augment matrix A with vector b
     Ab = [A[i] + [b[i]] for i in range(n)]
-
-    # Elimination phase
+    #Elimination phase
     for i in range(n):
-        # Find the row with the largest pivot
+        #Find row with largest pivot
         max_row = i
         for j in range(i + 1, n):
             if abs(Ab[j][i]) > abs(Ab[max_row][i]):
                 max_row = j
-        # Swap the current row with the row with the largest pivot
+        #Swap current row with row containing largest pivot
         Ab[i], Ab[max_row] = Ab[max_row], Ab[i]
-
-        # Eliminate the entries below the pivot
+        #Eliminate entries below pivot
         for j in range(i + 1, n):
             factor = Ab[j][i] / Ab[i][i]
             for k in range(i + 1, n + 1):
                 Ab[j][k] -= factor * Ab[i][k]
             Ab[j][i] = 0
 
-    # Back substitution phase
+    #Back substitution phase
     x = [0] * n
     for i in range(n - 1, -1, -1):
         x[i] = Ab[i][n]
@@ -59,4 +55,4 @@ def gaussian_elimination(A, b):
 
 sol = gaussian_elimination(A, b)
 
-print(sol)  # Output: [1.0, 2.0, 3.0]
+print(sol)
