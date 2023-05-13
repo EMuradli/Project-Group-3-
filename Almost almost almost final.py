1a7eda32ef0809ce6038cd8d6c41372966591d45
sennaTR <97544167+sennaTR@users.noreply.github.com>
2023-05-13 22:32:34 +0200

    Add files via upload

---
+++ Almost almost almost final.py
@@ -0,0 +1,156 @@
+import re
+from fractions import Fraction as frac
+from math import gcd
+import chemparse as chem
+
+print('\nIMPORTANT: This program is case sensitive, thus:'
+      '\n- Elements and compounds must be provided correctly (i.e. C, Cu, O2, CH4). '
+      '\n- An unbalanced equation as must be provided as follows: H2 + O2 -> H2O'
+      '\n- If there is a factored compound, simplify (i.e. (NH4)2 should be N2H8).'
+      '\n- The program will recognise random letters and words as elements; ensure '
+      'they are written correctly to avoid confusion.'
+      '\nOtherwise, the program may yield incorrect results or malfunction.\n')
+
+ListOfElements = []
+MatrixOfElements = []
+equation = input('Provide the equation: ').replace(' ', '').replace(',', '')
+reactants, products = equation.split("->")
+reactants = reactants.replace(' ', '').split("+")
+products = products.replace(' ', '').split("+")
+
+
+def augment_matrix(element, index, count, side):
+    if index == len(MatrixOfElements):
+        MatrixOfElements.append([])
+        for _ in ListOfElements:
+            MatrixOfElements[index].append(0)
+    if element not in ListOfElements:
+        ListOfElements.append(element)
+        for i in range(len(MatrixOfElements)):
+            MatrixOfElements[i].append(0)
+    column = ListOfElements.index(element)
+    MatrixOfElements[index][column] += count * side
+
+
+def retrieve_elements(segment, index, multiplier, side):
+    elementsAndNumbers = re.split('([A-Z][a-z]?)', segment)
+    i = 0
+    while i < len(elementsAndNumbers) - 1:  # last element always blank
+        i += 1
+        if len(elementsAndNumbers[i]) > 0:
+            if elementsAndNumbers[i + 1].isdigit():
+                count = int(elementsAndNumbers[i + 1]) * multiplier
+                augment_matrix(elementsAndNumbers[i], index, count, side)
+                i += 1
+            else:
+                augment_matrix(elementsAndNumbers[i], index, multiplier, side)
+
+
+def separate_elements(compound, index, side):
+    segments = re.split('(\([A-Za-z0-9]*\)[0-9]*)', compound)
+    for segment in segments:
+        if segment.startswith('('):
+            segment = re.split('\)([0-9]*)', segment)
+            elementsParse = chem.parse_formula(segment)
+            multiplier = int(elementsParse.values())
+            segment = segment[0][1:]
+        else:
+            multiplier = 1
+        retrieve_elements(segment, index, multiplier, side)
+
+
+for i in range(len(reactants)):
+    separate_elements(reactants[i], i, 1)
+for i in range(len(products)):
+    separate_elements(products[i], i + len(reactants), -1)
+
+tempMatrix = MatrixOfElements
+A = [list(x) for x in zip(*tempMatrix)]
+
+
+def largest_row_pivot(A, k):
+    max_row = k
+    for i in range(k + 1, len(A)):
+        if abs(A[i][k]) > abs(A[i - 1][k]):
+            max_row = i
+    return max_row
+
+
+def elimination_phase(A):
+    m, n = len(A), len(A[0])
+    for k in range(min(m, n)):
+        max_row = largest_row_pivot(A, k)
+        if A[max_row][k] == 0:
+            break
+        A[k], A[max_row] = A[max_row], A[k]
+        for i in range(k + 1, m):
+            c = A[i][k] / A[k][k]
+            for j in range(k + 1, n):
+                A[i][j] = A[i][j] - c * A[k][j]
+            A[i][k] = 0
+
+
+def backward_substitution_phase(A):
+    m, n = len(A), len(A[0])
+    for k in range(min(m, n) - 1, 0, -1):
+        if A[k][k] == 0:
+            continue
+        for i in range(k - 1, -1, -1):
+            c = A[i][k] / A[k][k]
+            for j in range(i, n):
+                A[i][j] = A[i][j] - c * A[k][j]
+    for i in range(min(m, n)):
+        if A[i][i] == 0:
+            continue
+        for j in range(n - 1, i - 1, -1):
+            A[i][j] = A[i][j] / A[i][i]
+
+
+def gaussian_elimination():
+    elimination_phase(A)
+    backward_substitution_phase(A)
+
+
+val3Floats = []
+val3Fracs = []
+
+gaussian_elimination()
+
+matrixArray = A
+for i, row in enumerate(matrixArray):
+    if i == len(matrixArray) - 1 and matrixArray[i][-1] == 0:
+        continue
+    elif matrixArray[i][i] == 0:
+        continue
+    else:
+        val3 = float(row[-1])
+        val3 *= -1
+        val3Floats.append(val3)
+        val3 = frac(val3).limit_denominator()
+        val3Fracs.append(val3.denominator)
+
+lcm = 1
+for i in val3Fracs:
+    lcm = lcm*i//gcd(lcm, i)
+
+freeVar = lcm
+varRes = []
+
+for i in range(len(val3Floats)):
+    res = val3Floats[i] * freeVar
+    varRes.append(int(res))
+
+varRes.append(freeVar)
+
+balanced = ''
+for i in range(len(reactants)):
+    balanced += str(varRes[i]) + reactants[i]
+    if i < len(reactants) - 1:
+        balanced += ' + '
+balanced += ' -> '
+
+for i in range(len(products)):
+    balanced += str(varRes[i + len(reactants)]) + products[i]
+    if i < len(products) - 1:
+        balanced += ' + '
+print(balanced)
